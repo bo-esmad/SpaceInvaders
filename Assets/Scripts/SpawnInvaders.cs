d1f92cade81ce5fae5c39911bf90db6839760d91
bruno o <40021775+bo-esmad@users.noreply.github.com>
2021-03-24 16:25:12 +0000

    Initial commit

---
+++ Assets/Scripts/SpawnInvaders.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnInvaders : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject invasorA;
+
+    [SerializeField]
+    GameObject invasorB;
+
+    [SerializeField]
+    int nInvasores = 7;
+
+    [SerializeField]
+    float xMin = -3f;
+
+    void Awake()
+    {
+        /*
+         * "Pega" neste objecto, duplica e coloca-o "naquele" sítio
+         */
+
+        float x = xMin;
+        for( int i = 1; i <= nInvasores; i++ )
+        {
+            GameObject newInvader = Instantiate(invasorA, transform);
+            newInvader.transform.position = new Vector3(x, -0.5f, 0f);
+            x += 1f;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# SpaceInvaders
+ Space Invaders - in the style of Programação I 2020/2021
